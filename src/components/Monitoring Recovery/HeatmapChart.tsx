d1c4b11dcc1c68c9809a485c3e96bed3c4d70905
RupaliMasal1996 <167097288+RupaliMasal1996@users.noreply.github.com>
2024-09-30 12:22:06 +0530

    Heatmap changes added.

--- src/components/Monitoring Recovery/HeatmapChart.tsx
+++ src/components/Monitoring Recovery/HeatmapChart.tsx
@@ -12,40 +12,40 @@ const HeatmapChart = ({ categoriesMatricHeatMap }: Props) => {
   const state: any = {
     series: {
       Location: [
-        { name: "DL", data: [70, 30, 50, 90, 30, 20, 50, 70, 60, 40, 90, 20] },
-        { name: "HR", data: [30, 50, 20, 70, 60, 90, 80, 40, 100, 20, 50, 30] },
-        { name: "KA", data: [60, 20, 30, 50, 70, 80, 90, 30, 20, 40, 60, 60] },
-        { name: "MP", data: [10, 20, 70, 50, 60, 90, 40, 30, 50, 50, 80, 100] },
-        { name: "TN", data: [90, 100, 30, 50, 60, 40, 30, 50, 70, 80, 20, 50] },
-        { name: "MH", data: [20, 30, 90, 50, 70, 80, 60, 40, 100, 70, 20, 30] },
-        { name: "UP", data: [30, 20, 70, 50, 40, 90, 60, 80, 70, 100, 30, 20] },
+        { name: "DL", data: [70, 30, 50, 90, 30, 20, 50, 70, 60, 40, 90, 20,10,40,60,80,90] },
+        { name: "HR", data: [20,10,40,60,80,30, 50, 20, 70, 60, 90, 80, 40, 100, 20, 50, 30] },
+        { name: "KA", data: [60, 20, 30, 50, 70, 80, 90,20,10,40,60,80, 30, 20, 40, 60, 60] },
+        { name: "MP", data: [10, 20, 70,20,10,40,60,80, 50, 60, 90, 40, 30, 50, 50, 80, 100] },
+        { name: "TN", data: [90, 100, 30, 50, 60, 40, 30, 50, 70,20,10,40,60,80, 80, 20, 50] },
+        { name: "MH", data: [20,10,40,60,80,20, 30, 90, 50, 70, 80, 60, 40, 100, 70, 20, 30] },
+        { name: "UP", data: [30, 20, 70, 50, 40, 90, 60, 80, 70, 100, 30, 20,20,10,40,60,80,] },
       ],
       POS: [
         {
           name: "<1L",
-          data: [80, 40, 50, 80, 50, 10, 50, 70, 60, 40, 90, 10],
+          data: [80, 40, 50, 80, 50, 10, 50, 70, 60, 40, 90, 10,20,10,40,60,80,],
         },
         {
           name: "1-5L",
-          data: [40, 60, 30, 20, 70, 60, 90, 80, 40, 100, 20, 50],
+          data: [20,10,40,60,80,40, 60, 30, 20, 70, 60, 90, 80, 40, 100, 20, 50],
         },
         {
           name: "5-10L",
-          data: [40, 30, 30, 50, 80, 90, 40, 50, 20, 10, 40, 10],
+          data: [40, 30, 30, 50, 80,20,10,40,60,80, 90, 40, 50, 20, 10, 40, 10],
         },
         {
           name: ">10L",
-          data: [60, 80, 50, 60, 90, 40, 30, 20, 40, 80, 100, 40],
+          data: [60, 80, 50, 60, 90, 40, 30, 20, 40,20,10,40,60,80, 80, 100, 40],
         },
       ],
       Vintage: [
-        { name: "V1", data: [90, 40, 50, 70, 90, 20, 80, 40, 70, 60, 40, 90] },
-        { name: "V2", data: [40, 60, 20, 60, 90, 80, 40, 40, 100, 30, 60, 50] },
-        { name: "V3", data: [40, 30, 30, 40, 90, 80, 40, 20, 10, 70, 10, 40] },
-        { name: "V4", data: [60, 50, 80, 50, 70, 90, 80, 40, 20, 40, 80, 10] },
-        { name: "V5", data: [70, 100, 20, 80, 90, 80, 60, 40, 70, 80, 30, 60] },
-        { name: "V6", data: [40, 30, 90, 90, 90, 80, 70, 40, 100, 70, 60, 30] },
-        { name: "V7", data: [70, 20, 70, 40, 20, 90, 80, 70, 70, 70, 90, 30] },
+        { name: "V1", data: [20,10,40,60,80,90, 40, 50, 70, 90, 20, 80, 40, 70, 60, 40, 90] },
+        { name: "V2", data: [40, 60, 20, 60, 90, 80, 40, 40, 100, 30, 60, 50,20,10,40,60,80,] },
+        { name: "V3", data: [40, 30, 20,10,40,60,80,30, 40, 90, 80, 40, 20, 10, 70, 10, 40] },
+        { name: "V4", data: [60, 50, 80, 50, 70, 90, 80, 20,10,40,60,80,40, 20, 40, 80, 10] },
+        { name: "V5", data: [70, 100,20,10,40,60,80, 20, 80, 90, 80, 60, 40, 70, 80, 30, 60] },
+        { name: "V6", data: [40, 30, 90, 90, 90, 80, 20,10,40,60,80,70, 40, 100, 70, 60, 30] },
+        { name: "V7", data: [70, 20, 20,10,40,60,80,70, 40, 20, 90, 80, 70, 70, 70, 90, 30] },
       ],
     },
     options: {
@@ -62,9 +62,7 @@ const HeatmapChart = ({ categoriesMatricHeatMap }: Props) => {
       },
       plotOptions: {
         heatmap: {
-          shadeIntensity: 0,
-          // useFillColorAsStroke: true,
-          // reverseNegativeShade: false,
+          shadeIntensity: 0.5,
           colorScale: {
             ranges: [
               { from: 0, to: 30, color: "#00B050", name: "Low" }, // green
@@ -76,26 +74,38 @@ const HeatmapChart = ({ categoriesMatricHeatMap }: Props) => {
       },
       xaxis: {
         type: "category",
+        
         categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
+        
+          "Feb2023",
+          "Mar2023",
+          "Apr2023",
+          "May2023",
+          "Jun2023",
+          "Jul2023",
+          "Aug2023",
+          "Sep2023",
+          "Oct2023",
+          "Nov2023",
+          "Dec2023",
+          "Jan2024",
+          "Feb2024",
+          "Mar2024",
+          "Apr2024",
+          "May2024",
+          "Jun2024",
         ],
         labels: {
+          rotate: -70,  // Rotate the labels to -70 degrees
+          offsetX: 0,   // Adjust the horizontal offset
+          offsetY: 0,   // Adjust the vertical offset
           style: {
-            colors: "#3B414B",
-            fontSize: "12px",
-            fontFamily: ["DM Sans"],
+            colors: "#3B414B",     // Set the label color
+            fontSize: "12px",      // Set the font size
+            fontFamily: "DM Sans", // Set the font family
           },
+          // `rotateAlways` forces the rotation even when space is small
+          rotateAlways: true, 
         },
       },
       yaxis: {
