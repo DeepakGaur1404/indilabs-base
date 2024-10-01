5fdf3e995a715324cf426568958a400e8d509b0a
RupaliMasal1996 <167097288+RupaliMasal1996@users.noreply.github.com>
2024-10-01 18:29:30 +0530

    We have done API integration for Dashboard Optimus screen

--- src/components/DashboardRecoveryCardsNew/Execution.tsx
+++ src/components/DashboardRecoveryCardsNew/Execution.tsx
@@ -18,13 +18,12 @@ import {
 
 interface DataItem {
   name: string;
-  percentage: number;
+  percentage: any;
 }
 
 const datakey: DataItem[] = [
-  { name: "Payment", percentage: 5 },
-  { name: "Contacts", percentage: 10 },
-  { name: "No Contacts", percentage: 85 },
+  { name: "Payer", percentage: 6.457267386772787 },
+  { name: "Non Payer", percentage: 93.54273261322722 },
 ];
 
 // interface DataKey {
@@ -35,38 +34,92 @@ const datakey: DataItem[] = [
 
 const data = [
   {
-    name: "Jan",
-    uv: 5.3,
-    pv: 3.1,
+    name: "Jan2024",
+    settlement: 106408875.31,
+    "recovery-settlment": 5842593.0,
   },
   {
-    name: "Feb",
-    uv: 5.1,
-    pv: 3,
+    name: "Feb2024",
+    settlement: 106096191.75,
+    "recovery-settlment": 7924916.53,
   },
   {
-    name: "Mar",
-    uv: 3.8,
-    pv: 5.3,
+    name: "Mar2024",
+    settlement: 118404084.02999997,
+    "recovery-settlment": 6022190.0,
   },
   {
-    name: "Apr",
-    uv: 5.1,
-    pv: 3,
+    name: "Apr2024",
+    settlement: 81356821.93000002,
+    "recovery-settlment": 3968882.0,
   },
   {
-    name: "May",
-    uv: 4.9,
-    pv: 2.9,
+    name: "May2024",
+    settlement: 100396115.69000001,
+    "recovery-settlment": 6488336.37,
   },
   {
-    name: "Jun",
-    uv: 5.1,
-    pv: 3,
+    name: "Jun2024",
+    settlement: 87351346.91,
+    "recovery-settlment": 494285.0,
   },
 ];
 
-const COLORS = ["#E5E3ED", "#C9C4D9", "#776BA1"];
+const arrTicks = (data: any[]): number[] => {
+  let arr: number[] = [];
+  let maxNum = 0;
+
+  if (data && data.length > 0) {
+    // Compute the sums of settlement and recovery-settlement
+    const sums = data.map((item) => {
+      const settlement = item.settlement || 0;
+      const recoverySettlement = item["recovery-settlment"] || 0;
+      return settlement + recoverySettlement;
+    });
+
+    // Find the maximum sum
+    maxNum = Math.max(...sums);
+
+    // Define the number of ticks
+    const numberOfTicks = 5;
+    const stepSize = maxNum / (numberOfTicks - 1);
+
+    // Round up maxNum to the nearest stepSize
+    maxNum = Math.ceil(maxNum / stepSize) * stepSize;
+
+    // Generate tick values
+    for (let i = 0; i <= numberOfTicks; i++) {
+      arr.push(parseFloat((i * stepSize).toFixed(1)));
+    }
+  } else {
+    arr = [0];
+  }
+
+  return arr;
+};
+
+
+const formatNumberMillion = (num: any) => {
+  if (num === 0) {
+    return "0";
+  } else if (num >= 1e7) {
+    // 10 million and above
+    return (num / 1e6).toFixed(2);
+  } else if (num >= 1e6) {
+    // 1 million to 10 million
+    return (num / 1e6).toFixed(2);
+  } else if (num >= 1e5) {
+    // 100,000 to 1 million
+    return (num / 1e6).toFixed(2);
+  } else {
+    // Less than 100,000
+    return (num / 1e6).toFixed(2); // You can adjust this if needed
+  }
+};
+
+
+
+const COLORS = ["#C9C4D9", "#776BA1"];
 const CUSTOM_LEGEND_COLORS = ["black", "black", "black"];
 
 const Execution = () => {
@@ -134,26 +187,38 @@ const Execution = () => {
                 />
               ))}
             </Pie>
-            <Tooltip />
+            <Tooltip
+              formatter={(value, name) => {
+                const formattedValue =
+                  typeof value === "number" ? `${value.toFixed(2)}%` : value;
+                return [formattedValue, `${name}`];
+              }}
+            />
           </PieChart>
         </ResponsiveContainer>
-        <div className="flex items-center justify-center gap-2 -mt-6">
-          <div className="flex items-center gap-1">
+        <div className="flex items-center justify-center gap-3 -mt-6">
+          {/* <div className="flex items-center gap-1">
             <div className="w-[8px] h-[8px] bg-[#E5E3ED] rounded-xl"></div>
             <div className="text-[12px] font-[400] font-['DM Sans'] text-[#000000] customClassThird">
               Payment%: <span className="ml-[2px]">5%</span>
             </div>
-          </div>
+          </div> */}
           <div className="flex items-center gap-1">
             <div className="w-[8px] h-[8px] bg-[#C9C4D9] rounded-xl"></div>
             <div className="text-[12px] font-[400] font-['DM Sans'] text-[#000000] customClassThird">
-              Contacts%: <span className="ml-[2px]">10%</span>
+              Payer:{" "}
+              <span className="ml-[2px]">
+                {datakey[0].percentage.toFixed(2)}%
+              </span>
             </div>
           </div>
           <div className="flex items-center gap-1">
             <div className="w-[8px] h-[8px] bg-[#776BA1] rounded-xl"></div>
             <div className="text-[12px] font-[400] font-['DM Sans'] text-[#000000] customClassThird">
-              No Contacts%: <span className="ml-[2px]">85%</span>
+              Non Payer:{" "}
+              <span className="ml-[2px]">
+                {datakey[1].percentage.toFixed(2)}%
+              </span>
             </div>
           </div>
         </div>
@@ -173,18 +238,16 @@ const Execution = () => {
         <div className="flex flex-col items-center w-[100%]">
           <ResponsiveContainer
             width="100%"
-            height={280}
-            className="mr-12 mt-2 w-[100%]"
+            height={270}
+          
           >
             <BarChart
-              // width={400}
-              // height={100}
               data={data}
               margin={{
                 top: 5,
-                right: 3,
-                left: -3,
-                bottom: 5,
+                right: 0,
+                left: -20,
+                bottom: 20,
               }}
             >
               <CartesianGrid strokeDasharray="3 3" vertical={false} />
@@ -196,27 +259,47 @@ const Execution = () => {
                 fontSize={10}
                 fontFamily="DM Sans"
                 fill={"#3B414B"}
+                angle={-70}
+                dy={0}
+                dx={-4}
+                textAnchor="end"
               />
               <YAxis
                 axisLine={false}
                 tickLine={false}
-                domain={[0, 10]}
-                ticks={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+                domain={[0, (dataMax:any) => Math.ceil(dataMax)]}
+                tickFormatter={formatNumberMillion}
+                ticks={arrTicks(data)}
                 fontWeight={400}
                 fontSize={10}
                 fontFamily="DM Sans"
                 fill={"#3E4259"}
               />
-              <Tooltip />
+              <Tooltip
+                formatter={(value: any, name) => {
+                  const formattedValue =
+                    typeof value === "number" && name === "settlement"
+                      ? `${value.toFixed(1)}%`
+                      : typeof value === "number" &&
+                        name === "Recovery Settlement"
+                      ? `${value.toFixed(1)}%`
+                      : typeof value === "number"
+                      ? `${Math.floor(value).toLocaleString()}`
+                      : ` ${parseFloat(value.toFixed(2)).toLocaleString()}`;
+                  return [formattedValue, `${name}`];
+                }}
+              />
               <Bar
-                dataKey="pv"
+                dataKey="settlement"
+                name="Settlement"
                 stackId="a"
                 fill="#4339F2"
                 barSize={10}
                 radius={[0, 0, 10, 10]}
               />
               <Bar
-                dataKey="uv"
+                dataKey="recovery-settlment"
+                name="Recovery settlement"
                 stackId="a"
                 fill="#FFB200"
                 barSize={10}
@@ -225,17 +308,17 @@ const Execution = () => {
             </BarChart>
           </ResponsiveContainer>
 
-          <div className="flex items-center gap-5">
+          <div className="flex items-center gap-5 mt-2">
             <div className="flex items-center gap-1">
               <div className="w-[8px] h-[8px] bg-[#4339F2] rounded-xl"></div>
               <div className="text-[12px] font-[400] text-[#000000] font-['DM Sans'] customClassThird">
-                Within CL
+                Settlement
               </div>
             </div>
             <div className="flex items-center gap-1">
               <div className="w-[8px] h-[8px] bg-[#FFB200] rounded-xl"></div>
               <div className="text-[12px] text-[#000000] font-[400] font-['DM Sans'] customClassThird">
-                Exceed CL
+                Recovery Settlement
               </div>
             </div>
           </div>
